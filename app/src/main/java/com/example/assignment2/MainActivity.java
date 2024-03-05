976dbcca973c6a522b16811c442f9d3019f06d2f
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2024-03-05 12:16:15 -0500

    make weather pop-up, now need to feed news from CNN as lecture6
    make weather pop-up, now need to feed news from CNN as lecture6

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.assignment2;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +15,12 @@ import androidx.fragment.app.FragmentManager;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
+import android.app.AlertDialog;
+import android.os.Handler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout dl;
@@ -38,12 +46,14 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 String myText = null;
-                if(id == R.id.account) {
-                    myText = "Account";
-                } else if(id == R.id.settings) {
-                    myText = "Settings";
-                } else if(id == R.id.mycart) {
-                    myText = "My Cart";
+                if(id == R.id.list) {
+                    myText = "Saved List";
+                    showPopupList("Saved List");
+                } else if(id == R.id.weather) {
+                    myText = "Weather";
+                    showWeatherPopup();
+                } else if(id == R.id.News) {
+                    myText = "News";
                 } else {
                     return true;
                 }
@@ -55,9 +65,55 @@ public class MainActivity extends AppCompatActivity {
                 dl.closeDrawers();
                 return true;
             }
+
         });
 
 
+    }
+
+
+    private void showPopupList(String message) {
+        // 팝업을 생성하고 설정합니다.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+
+        // 팝업을 표시합니다.
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // 5초 후에 팝업을 닫습니다.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                dialog.dismiss();
+            }
+        }, 5000); // 5000 밀리초 = 5초
+    }
+
+    public void showWeatherPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Current Weather");
+        @SuppressLint("SetJavaScriptEnabled")
+
+        // WebView를 생성하고 설정합니다.
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        //webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        webView.loadUrl("https://weather.com/weather/today/l/c5326b3950212c71c67cee42b213313f555df223d9668aabb44c6719dc67685c");
+
+        // AlertDialog에 WebView를 추가합니다.
+        builder.setView(webView);
+
+        // AlertDialog를 표시합니다.
+        builder.setPositiveButton("close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss(); // AlertDialog를 닫습니다.
+            }
+        });
+        builder.show();
     }
 
     private void startMyFragment(Bundle args ) {
@@ -73,13 +129,16 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+    // 필요하면 사용하기
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 //        TODO: Start fragment
         int id = item.getItemId();
         String myText = null;
-        if(id == R.id.account) {
-            myText = "Account";
+        if(id == R.id.weather) {
+            myText = "Weather";
+
         } else if(id == R.id.settings) {
             myText = "Settings";
         }
